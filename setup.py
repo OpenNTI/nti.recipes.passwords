a172f8921c9418747d53e2214481cd39ff68b488
Jason Madden <jason.madden@nextthought.com>
2013-09-28 18:42:43 +0000

    making a place for password recipes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.recipes.passwords/trunk@24935 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import codecs
+
+VERSION = '0.0.0'
+
+entry_points = {
+	"zc.buildout" : [
+		'databases = nti.recipes.relstorage.meta:Databases'
+	],
+}
+
+setup(
+    name = 'nti.recipes.relstorage',
+    version = VERSION,
+    author = 'Jason Madden',
+    author_email = 'open-source@nextthought.com',
+    description = "zc.buildout recipes for RelStorage",
+    long_description = codecs.open('README.rst', encoding='utf-8').read(),
+    license = 'Proprietary',
+    keywords = 'buildout relstorage',
+    #url = 'https://github.com/NextThought/nti.nose_traceback_info',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Testing'
+		'Framework :: Buildout',
+        ],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti', 'nti.recipes'],
+	install_requires=[
+		'setuptools',
+		'zc.buildout',
+		'zc.recipe.deployment'
+	],
+	entry_points=entry_points
+)
