c8506172aecd75c86f6b7805b4fada39ad81ecde
Carlos Sanchez <papachoco@gmail.com>
2017-06-07 19:19:21 -0500

    add supporting files

--- README.rst
+++ README.rst
@@ -1,1 +1,9 @@
+=======================
+ nti.recipes.passwords
+=======================
 
+.. image:: https://travis-ci.org/NextThought/nti.recipes.passwords.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.recipes.passwords
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.recipes.passwords/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.recipes.passwords?branch=master
--- setup.py
+++ setup.py
@@ -4,47 +4,66 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"zc.buildout" : [
-		'default = nti.recipes.passwords:DecryptSection',
-		'decryptFile = nti.recipes.passwords:DecryptFile'
-	],
+    "zc.buildout": [
+        'default = nti.recipes.passwords:DecryptSection',
+        'decryptFile = nti.recipes.passwords:DecryptFile'
+    ],
 }
+
+TESTS_REQUIRE = [
+    'nti.testing',
+    'zope.testrunner',
+]
 
 import platform
 py_impl = getattr(platform, 'python_implementation', lambda: None)
 IS_PYPY = py_impl() == 'PyPy'
 
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
-	name = 'nti.recipes.passwords',
-	version = VERSION,
-	author = 'Jason Madden',
-	author_email = 'open-source@nextthought.com',
-	description = "zc.buildout recipes for securely storing passwords in version control",
-	long_description = codecs.open('README.rst', encoding='utf-8').read(),
-	license = 'Proprietary',
-	keywords = 'buildout password',
-	classifiers = [
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-		'Topic :: Software Development :: Testing'
-		'Framework :: Buildout',
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.recipes'],
-	install_requires=[
-		'setuptools',
-		'zc.buildout',
-		'zc.recipe.deployment',
-		# NOTE: We use Crypto but CANNOT depend on it, it's too late,
-		# it must be installed in the buildout. See __init__.py for details.
-		'pycrypto >= 2.6' if not IS_PYPY else ''
-	],
-	entry_points=entry_points
+    name='nti.recipes.passwords',
+    version=_read('version.txt').strip(),
+    author='Jason Madden',
+    author_email='open-source@nextthought.com',
+    description="zc.buildout recipes for securely storing passwords in version control",
+    long_description=(_read('README.rst') + '\n\n' + _read("CHANGES.rst")),
+    license='Apache',
+    keywords='buildout password',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Buildout',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.recipes'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'zc.recipe.deployment',
+        # NOTE: We use Crypto but CANNOT depend on it, it's too late,
+        # it must be installed in the buildout. See __init__.py for
+        # details.
+        'pycrypto >= 2.6' if not IS_PYPY else ''
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+    },
+    entry_points=entry_points,
+    test_suite="nti.recipes.passwords.tests",
 )
